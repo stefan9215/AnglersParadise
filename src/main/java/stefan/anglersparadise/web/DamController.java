77392f7d1b4ad2dff13a17488351f53352817fc5
Stefan Troshanov <stefantroshanov@yahoo.com>
2022-06-09 13:31:34 +0300

    Implemented all dams template.

---
+++ src/main/java/stefan/anglersparadise/web/DamController.java
@@ -0,0 +1,15 @@
+package stefan.anglersparadise.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DamController {
+
+    @GetMapping("/dams/all")
+    public String allDams(Model model) {
+
+        return "all-dams";
+    }
+}
