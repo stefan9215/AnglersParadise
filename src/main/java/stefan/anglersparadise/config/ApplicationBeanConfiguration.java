8ea38ff7a7c297e04667cb7d8709f0a9d0fff78b
Stefan Troshanov <stefantroshanov@yahoo.com>
2022-06-10 23:42:44 +0300

    Added ModelMapper as Bean.

--- src/main/java/stefan/anglersparadise/config/ApplicationBeanConfiguration.java
+++ src/main/java/stefan/anglersparadise/config/ApplicationBeanConfiguration.java
@@ -1,5 +1,6 @@
 package stefan.anglersparadise.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,5 +12,10 @@ public class ApplicationBeanConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Pbkdf2PasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
--- src/main/java/stefan/anglersparadise/web/UserLoginController.java
+++ src/main/java/stefan/anglersparadise/web/UserLoginController.java
@@ -3,12 +3,7 @@ package stefan.anglersparadise.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import stefan.anglersparadise.model.entity.UserEntity;
-import stefan.anglersparadise.model.test.UserLoginModel;
-import stefan.anglersparadise.repository.UserRepository;
 
-import java.util.Optional;
 
 @Controller
 public class UserLoginController {
