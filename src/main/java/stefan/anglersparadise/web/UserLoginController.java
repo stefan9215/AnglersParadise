99fa1ec8e2680c142e5ecb2b0fbc98c0da061ab0
Stefan Troshanov <stefantroshanov@yahoo.com>
2022-06-11 00:53:38 +0300

    Added server validation to login page.

--- src/main/java/stefan/anglersparadise/web/UserLoginController.java
+++ src/main/java/stefan/anglersparadise/web/UserLoginController.java
@@ -1,17 +1,32 @@
 package stefan.anglersparadise.web;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 public class UserLoginController {
-
 
     @GetMapping("/users/login")
     public String login(Model model) {
         return "login";
     }
 
+
+    @PostMapping("/users/login-error")
+    public String loginError(@ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                             String username
+            , RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+        redirectAttributes.addFlashAttribute("username",username);
+
+        return "redirect:/users/login";
+    }
 }
